3023ffb3ebcc7536adb99c2b16ff565679e99762
hujiacheng <hujiacheng@iipcloud.onaliyun.com>
2025-08-14 12:23:26 +0800

    feat: 更新 IipTable 组件，增强插槽功能，支持自定义列和表头插槽，优化类型定义以提升可扩展性

--- packages/components/src/components/table/table.vue
+++ packages/components/src/components/table/table.vue
@@ -9,7 +9,9 @@
         v-bind="checkBoxColumnConfig.tableColumnProps"
       >
         <template #default="slotData">
-          <slot name="checkbox-slot-column" v-bind="slotData"></slot>
+          <template v-if="getCheckBoxSlot">
+            <slot name="checkbox-slot-column-default" v-bind="slotData"></slot>
+          </template>
         </template>
       </vxe-column>
       <!-- 数据列 -->
@@ -19,11 +21,19 @@
         v-bind="column.tableColumnProps"
       >
         <template #default="slotData">
-          <template v-if="column.slotName">
-            <slot :name="column.slotName + '-column'" v-bind="slotData"></slot>
+          <template v-if="getSlotKey(column) in getColumnSlot('default')">
+            <slot :name="getSlotKey(column)" v-bind="slotData"></slot>
           </template>
           <template v-else>
             {{ slotData.row[column.tableColumnProps.field || ''] }}
+          </template>
+        </template>
+        <template #header="slotData">
+          <template v-if="getSlotKey(column) in getColumnSlot('header')">
+            <slot :name="getSlotKey(column)" v-bind="slotData"></slot>
+          </template>
+          <template v-else>
+            {{ slotData.column.title }}
           </template>
         </template>
       </vxe-column>
@@ -37,22 +47,42 @@
 </template>
 
 <script setup lang="ts">
-import { ref, computed, toRefs, useAttrs } from 'vue'
+import { ref, computed, toRefs, useAttrs, useSlots } from 'vue'
 import { VxeTable, VxeColumn, VxeTableInstance } from 'vxe-table'
 import { VxePager } from 'vxe-pc-ui'
-import type { IipTableProps, IipTableExpose } from './types'
+import type { IipTableProps, IipTableExpose, TableColumn } from './types'
 import 'vxe-pc-ui/es/style.css'
 defineOptions({
   name: 'IipTable'
 })
+const slots = useSlots()
 const attrs = useAttrs()
 const props = defineProps<IipTableProps>()
-
+// 解构 props
+const { pagination, columns, checkBoxColumnConfig } = toRefs<IipTableProps>(props)
+const getColumnSlot = (slotType: string) => {
+  const columnSlot = columns?.value?.filter(column => column.slotConfig?.slotType === slotType)
+  if (columnSlot && columnSlot.length > 0) {
+    const defaultColumnSlotMap: Record<string, any> = {}
+    columnSlot.forEach(column => {
+      const slotKey = getSlotKey(column)
+      if (slots[slotKey]) {
+        defaultColumnSlotMap[slotKey] = slots[slotKey]
+      }
+    })
+    return defaultColumnSlotMap
+  }
+  return {}
+}
+const getSlotKey = (column: TableColumn) => {
+  return `${column.tableColumnProps.field}-slot-${column.slotConfig?.slotArea}-${column.slotConfig?.slotType}`
+}
+const getCheckBoxSlot = computed(() => {
+  return slots['checkbox-slot-column-default']
+})
 // 表格实例
 const tableRef = ref()
 const tableInstance = computed(() => tableRef.value)
-// 解构 props
-const { pagination, columns, checkBoxColumnConfig } = toRefs<IipTableProps>(props)
 
 defineExpose<IipTableExpose>({
   getTableInstance: () => tableInstance.value as VxeTableInstance
