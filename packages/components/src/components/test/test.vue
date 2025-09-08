85695c03feee7d2c117d26c4b74efa3736d05a2f
hujiacheng <hujiacheng@iipcloud.onaliyun.com>
2025-09-08 18:36:44 +0800

    feat: 在项目中新增测试组件 test.vue，并更新 lint-staged 配置以自动修复代码格式

---
+++ packages/components/src/components/test/test.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="test">
+    <h1>Test</h1>
+  </div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped>
+.test {
+  width: 100%;
+  height: 100%;
+  background-color: #fff;
+}
+</style>
