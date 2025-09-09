0a41fa372810ad9e89599d68b75a7b80a076ae9e
hujiacheng <hujiacheng@iipcloud.onaliyun.com>
2025-09-09 18:05:05 +0800

    chore: 删除主题提供者和切换器组件及其相关类型定义，简化组件结构

--- packages/components/src/components/theme-provider/index.ts
+++
@@ -1,7 +0,0 @@
-import { withInstall } from '@bingwu/iip-ui-utils'
-import ThemeProvider from './theme-provider.vue'
-
-export const IipThemeProvider = withInstall(ThemeProvider)
-export default IipThemeProvider
-
-export * from './types'
--- packages/components/src/components/theme-switcher/index.ts
+++
@@ -1,7 +0,0 @@
-import { withInstall } from '@bingwu/iip-ui-utils'
-import ThemeSwitcher from './theme-switcher.vue'
-
-export const IipThemeSwitcher = withInstall(ThemeSwitcher)
-export default IipThemeSwitcher
-
-export * from './types'
