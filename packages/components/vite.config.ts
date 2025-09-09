816ccc1634060ff8fda7facf94a3b4d15d33ab36
hujiacheng <hujiacheng@iipcloud.onaliyun.com>
2025-09-09 18:19:06 +0800

    chore: 更新外部依赖名称，调整组件配置

--- packages/components/vite.config.ts
+++ packages/components/vite.config.ts
@@ -45,7 +45,7 @@ export default defineConfig({
     },
     rollupOptions: {
       // 确保外部化处理那些你不想打包进库的依赖
-      external: ['vue', 'element-plus', '@iip-ui/utils', '@iip-ui/theme/dist/index.css'],
+      external: ['vue', 'element-plus', '@bingwu/iip-ui-utils', '@bingwu/iip-ui-theme'],
       output: {
         // 在 UMD 构建模式下为这些外部化的依赖提供一个全局变量
         globals: {
