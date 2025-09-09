b1c5fb5b494a29a2a52f3a6092212c77a79da3cb
hujiacheng <hujiacheng@iipcloud.onaliyun.com>
2025-09-09 18:09:55 +0800

    chore: 继续移除未使用的主题相关组件导入，进一步优化组件类型导出

--- packages/components/src/types/index.ts
+++ packages/components/src/types/index.ts
@@ -27,5 +27,3 @@ export interface BaseProps {
 // 导出所有组件类型
 export * from '../components/input/types'
 export * from '../components/select/types'
-export * from '../components/theme-provider/types'
-export * from '../components/theme-switcher/types'
