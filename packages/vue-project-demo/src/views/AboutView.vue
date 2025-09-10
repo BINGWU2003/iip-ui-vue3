f8c745bcf19f2c9a9db5303b08ab672be1349063
hujiacheng <hujiacheng@iipcloud.onaliyun.com>
2025-09-10 11:59:44 +0800

    chore: 更新 vue-project-demo，添加 ElementPlus、VxeTable 和 VxePCUI，修改 ESLint 配置，优化 AboutView 和 HomeView

--- packages/vue-project-demo/src/views/AboutView.vue
+++ packages/vue-project-demo/src/views/AboutView.vue
@@ -1,7 +1,95 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
-  </div>
+  <iip-form
+    :model="conditionalModel"
+    :form-items="conditionalItems"
+    label-width="100px"
+    @submit="handleSubmit"
+  />
 </template>
 
-<style></style>
+<script setup lang="ts">
+import { reactive, computed } from 'vue'
+import type { FormItemConfig } from '@bingwu/iip-ui-components'
+
+const conditionalModel = reactive({
+  userType: '',
+  companyName: '',
+  personalName: '',
+  hasExperience: false,
+  experience: 0,
+})
+
+const conditionalItems = computed((): FormItemConfig[] => [
+  {
+    formItemProps: { label: '用户类型', prop: 'userType', required: true },
+    colProps: { span: 24 },
+    show: true,
+    componentProps: {
+      type: 'select',
+      formItemKey: 'userType',
+      itemProps: {
+        placeholder: '请选择用户类型',
+        options: [
+          { label: '个人用户', value: 'personal' },
+          { label: '企业用户', value: 'company' },
+        ],
+      },
+    },
+  },
+  {
+    formItemProps: { label: '姓名', prop: 'personalName', required: true },
+    colProps: { span: 24 },
+    show: conditionalModel.userType === 'personal', // 只有选择个人用户时才显示
+    componentProps: {
+      type: 'input',
+      formItemKey: 'personalName',
+      itemProps: {
+        placeholder: '请输入姓名',
+      },
+    },
+  },
+  {
+    formItemProps: { label: '公司名称', prop: 'companyName', required: true },
+    colProps: { span: 24 },
+    show: conditionalModel.userType === 'company', // 只有选择企业用户时才显示
+    componentProps: {
+      type: 'input',
+      formItemKey: 'companyName',
+      itemProps: {
+        placeholder: '请输入公司名称',
+      },
+    },
+  },
+  {
+    formItemProps: { label: '是否有工作经验', prop: 'hasExperience' },
+    colProps: { span: 24 },
+    show: conditionalModel.userType === 'personal', // 只有个人用户才显示
+    componentProps: {
+      type: 'switch',
+      formItemKey: 'hasExperience',
+      itemProps: {
+        activeText: '有',
+        inactiveText: '无',
+      },
+    },
+  },
+  {
+    formItemProps: { label: '工作年限', prop: 'experience' },
+    colProps: { span: 24 },
+    show: conditionalModel.userType === 'personal' && conditionalModel.hasExperience, // 个人用户且有工作经验才显示
+    componentProps: {
+      type: 'number',
+      formItemKey: 'experience',
+      itemProps: {
+        min: 0,
+        max: 50,
+        placeholder: '请输入工作年限',
+      },
+    },
+  },
+])
+
+const handleSubmit = (data: Record<string, any>) => {
+  console.log('表单数据:', data)
+}
+</script>
--- packages/vue-project-demo/src/views/HomeView.vue
+++ packages/vue-project-demo/src/views/HomeView.vue
@@ -1,4 +1,12 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+// iip-ui-utils 工具函数
+import { EovaToAvueConverter } from '@bingwu/iip-ui-utils'
+import { onMounted } from 'vue'
+
+onMounted(() => {
+  console.log(EovaToAvueConverter)
+})
+</script>
 
 <template>
   <main class="test">home</main>
