0c9e478d715cf3b1c3e0a61add408a8410d8430e
hujiacheng <hujiacheng@iipcloud.onaliyun.com>
2025-09-24 15:14:19 +0800

    fix: 修复 splitSlots 函数的类型定义，将 Object 类型更改为 object 类型

--- packages/components/src/utils/tools/index.ts
+++ packages/components/src/utils/tools/index.ts
@@ -1,5 +1,5 @@
 import type { Prefixed } from '../types'
-export const splitSlots = (slots: Prefixed<Object, string>, prefix: string) => {
+export const splitSlots = (slots: Prefixed<object, string>, prefix: string) => {
   return Object.keys(slots || {})
     .filter(key => key.startsWith(prefix + '-'))
     .map(key => key.split('-')[1])
