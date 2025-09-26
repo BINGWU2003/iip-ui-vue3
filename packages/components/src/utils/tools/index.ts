2fbd715ebfadb00906b84ed1765fe2af84cd2661
hujiacheng <hujiacheng@iipcloud.onaliyun.com>
2025-09-26 14:47:16 +0800

    chore: 更新 splitSlots 函数，优化键处理逻辑
    - 修改 splitSlots 函数，优化键的处理方式，移除前缀后返回剩余部分，提升代码可读性。

--- packages/components/src/utils/tools/index.ts
+++ packages/components/src/utils/tools/index.ts
@@ -2,5 +2,9 @@ import type { Prefixed } from '../types'
 export const splitSlots = (slots: Prefixed<object, string>, prefix: string) => {
   return Object.keys(slots || {})
     .filter(key => key.startsWith(prefix + '-'))
-    .map(key => key.split('-')[1])
+    .map(key => {
+      const keyArr = key.split('-')
+      keyArr.shift()
+      return keyArr.join('-')
+    })
 }
