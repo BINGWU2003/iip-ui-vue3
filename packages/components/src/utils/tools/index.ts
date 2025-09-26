4424587812007baa87f7b532d29ec00848db2ff2
hujiacheng <hujiacheng@iipcloud.onaliyun.com>
2025-09-26 15:07:59 +0800

    chore: 更新 splitSlots 函数，添加详细注释
    - 在 splitSlots 函数中添加了详细的注释，说明函数的用途、参数和返回值，提升代码可读性和使用指导。

--- packages/components/src/utils/tools/index.ts
+++ packages/components/src/utils/tools/index.ts
@@ -1,10 +1,15 @@
 import type { Prefixed } from '../types'
+/**
+ * 拆分插槽 用于二次封装组件时，获取不同字段的插槽
+ * 注意：如果该组件本身原有的插槽包含分隔符 - ,则该方法不适合使用
+ * 例如: 组件el-image 的插槽名称包含 - ,比如为viewer-error 等,因为该方法会拆分出 viewer 和 error 两个插槽
+ * 组件文档:https://element-plus.org/zh-CN/component/image#image-viewer-slots
+ * @param slots 插槽对象
+ * @param prefix 前缀
+ * @returns 拆分后的插槽
+ */
 export const splitSlots = (slots: Prefixed<object, string>, prefix: string) => {
   return Object.keys(slots || {})
     .filter(key => key.startsWith(prefix + '-'))
-    .map(key => {
-      const keyArr = key.split('-')
-      keyArr.shift()
-      return keyArr.join('-')
-    })
+    .map(key => key.split('-').pop())
 }
