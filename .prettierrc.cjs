c67da1dd8d10dd94be21c4b09377bd5803368b7d
hujiacheng <hujiacheng@iipcloud.onaliyun.com>
2025-11-11 14:34:29 +0800

    chore: 添加 ESLint 和 Prettier 配置，优化代码质量管理
    - 新增 .eslintrc.cjs 和 .prettierrc.cjs 配置文件，替代旧的 .eslintrc.json 和 .prettierrc.yml 文件，提升代码质量管理的一致性。
    - 新增 .eslintignore 文件，定义需要忽略的文件和目录，优化 ESLint 的运行效率。
    - 移除不再使用的配置文件，保持项目结构的整洁。

---
+++ .prettierrc.cjs
@@ -0,0 +1,14 @@
+module.exports = {
+  semi: false,
+  singleQuote: true,
+  printWidth: 100,
+  tabWidth: 2,
+  useTabs: false,
+  trailingComma: 'none',
+  bracketSpacing: true,
+  bracketSameLine: false,
+  arrowParens: 'avoid',
+  endOfLine: 'auto',
+  vueIndentScriptAndStyle: false,
+  singleAttributePerLine: false
+}
