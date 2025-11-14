7227632e1f3d7b2039f20d12baf5913ae6e60351
hujiacheng <hujiacheng@iipcloud.onaliyun.com>
2025-11-14 11:37:39 +0800

    chore: 更新 Vite 和 tsconfig 配置，优化构建设置
    - 在 components 的 Vite 配置中移除不必要的外部依赖 '@bingwu/iip-ui-theme'，并禁用源码映射以简化构建输出。
    - 在 utils 的 tsconfig.json 中调整 include 和 exclude 字段的格式，提升可读性和一致性。

--- packages/components/vite.config.ts
+++ packages/components/vite.config.ts
@@ -50,7 +50,7 @@ export default defineConfig({
     },
     rollupOptions: {
       // 确保外部化处理那些你不想打包进库的依赖
-      external: ['vue', 'element-plus', '@bingwu/iip-ui-utils', '@bingwu/iip-ui-theme'],
+      external: ['vue', 'element-plus', '@bingwu/iip-ui-utils'],
       output: {
         // 在 UMD 构建模式下为这些外部化的依赖提供一个全局变量
         globals: {
@@ -63,7 +63,7 @@ export default defineConfig({
       }
     },
     // 生成源码映射
-    sourcemap: true,
+    sourcemap: false,
     // 清空输出目录
     emptyOutDir: true
   }
