c286049ce0123eb1159ff81f804955ba5129b0cd
hujiacheng <hujiacheng@iipcloud.onaliyun.com>
2025-12-19 16:28:33 +0800

    feat: 新增 button-dropdown 组件
    - 创建了 button-dropdown.vue 组件，包含可插槽的标题和内容部分，提升了组件库的功能性和灵活性。
    - 该组件使用了 Element Plus 的 el-button 和 el-dropdown，便于用户进行下拉选择操作。

---
+++ packages/components/src/components/button-dropdown/button-dropdown.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <el-button>
+      <slot name="title"></slot>
+    </el-button>
+    <el-dropdown>
+      <slot name="content"></slot>
+    </el-dropdown>
+  </div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped></style>
